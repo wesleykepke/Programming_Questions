7d25db5a6e092820d6fd78f21181986cde50a8c5
Wesley Kepke <wesleykepke@nevada.unr.edu>
2015-05-19 15:05:45 -0700

    Updated documentation on 1_8.cpp

--- arrays_and_strings/1_8.cpp
+++ arrays_and_strings/1_8.cpp
@@ -1,7 +1,7 @@
 /*
 Name: Wesley Kepke
 Date: May 19, 2015
-Problem: Assume you have a method "isSubstring" which checks if one one is a 
+Problem: Assume you have a method "isSubstring" which checks if one string is a 
 substring of another. Given two strings, s1 and s2, write code to check if s2
 is a rotation of s1 using only one call to "isSubstring" (e.g., "waterbottle"
 is a rotation of "erbottlewat").
